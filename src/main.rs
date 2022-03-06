3b1babb0d30b881d65ca7f342e8e77afa43f51cb
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-03-06 22:05:28 +0900

    できた！！！！

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use std::str::FromStr;
+use yew::prelude::*;
+use web_sys::HtmlInputElement;
+struct Gcd {
+    ans: usize,
+    intex: String,
+}
+
+enum Message {
+    Inputted(String),
+    Run,
+}
+
+impl Component for Gcd {
+    type Message = Message;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            intex: "1x1".to_string(),
+            ans: 0,
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Message::Inputted(n)=>{
+                self.intex=match n.trim().parse(){
+                    Ok(ok)=>ok,
+                    Err(_)=>String::default(),
+                };
+                false
+            }
+            Message::Run => {
+                let intext = parse_pair(&self.intex, 'x').expect("parse error");
+                let mut n = intext.0;
+                let mut m = intext.1;
+                log::info!("intext.0 {}",intext.0);
+                log::info!("intext.1 {}",intext.1);
+                if n != 0 && m != 0 {
+                    while m != 0 {
+                        if m < n {
+                            std::mem::swap(&mut m, &mut n);
+                        }
+                        m %= n;
+                        log::info!("n:{}",n);
+                        log::info!("m:{}",m);
+                    }
+                }
+                self.ans = n;
+                log::info!("ans:{}",n);
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+            <div>
+                <input type="text" oninput={ctx.link().callback(|e: InputEvent| Message::Inputted(e.target_unchecked_into::<HtmlInputElement>().value()))} />
+                <button onclick={ctx.link().callback(|_| Message::Run)}>{"Run"}</button>
+            </div>
+            <div>
+                {self.ans}
+            </div>
+            </div>
+        }
+    }
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+fn main() {
+    yew::start_app::<Gcd>();
+    wasm_logger::init(wasm_logger::Config::default());
+}
